3a206ed9e21a3469af583c058abb94a93fa84cf4
antonpanchishko <PanchishkoMayor@gmail.com>
2021-01-14 19:56:37 +0200

    13) fixed initDb.sql

--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -3,7 +3,7 @@ CREATE TABLE `manufacturer` (
                                 `manufacturer_id` bigint NOT NULL AUTO_INCREMENT,
                                 `manufacturer_name` varchar(150) NOT NULL,
                                 `manufacturer_country` varchar(45) NOT NULL,
-                                `isDeleted` tinyint NOT NULL DEFAULT '0',
+                                `deleted` tinyint NOT NULL DEFAULT '0',
                                 PRIMARY KEY (`manufacturer_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
