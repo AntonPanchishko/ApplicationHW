c0daa85601c52a68265e08d81e4477f360e4ef7f
antonpanchishko <PanchishkoMayor@gmail.com>
2021-01-26 10:40:05 +0200

    35) passed checkstyle

--- src/main/java/taxi/security/AuthenticationServiceImpl.java
+++ src/main/java/taxi/security/AuthenticationServiceImpl.java
@@ -1,12 +1,11 @@
 package taxi.security;
 
+import java.util.Optional;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.lib.Service;
 import taxi.model.Driver;
 import taxi.service.DriverService;
-
-import java.util.Optional;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
